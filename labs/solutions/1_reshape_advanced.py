c49bf78ba17dbe11672dbe9cbcc9e6ae7f07394b
Georg Heiler <georg.kf.heiler@gmail.com>
2019-11-26 08:26:54 +0100

    improvements after upgrading osx

--- labs/solutions/1_reshape_advanced.py
+++ labs/solutions/1_reshape_advanced.py
@@ -5,4 +5,23 @@ res['val'] = results.loc[results.index!='Intercept',['Coef.', 'P>|t|', 't']].app
 res['model'] = results.loc[results.index!='Intercept',['model']]
 # display(res.head())
 
-res.pivot_table('val', res.index, ['variant', 'model'], lambda x: x)
+res = res.pivot_table('val', res.index, ['variant', 'model'], lambda x: x)
+display(res)
+
+def highlight_significant(x, sign_level_1, sign_level_2):
+    if x is np.nan:
+        return ''
+    else:
+        if isinstance(x, list):
+            p_value = float(x[2])
+            if p_value < sign_level_2:
+                return 'font-weight: bold;background-color: lightgreen'
+            elif p_value < sign_level_1:
+                color = 'lightgreen'
+                return 'background-color: %s' % color
+            else:
+                return ''
+        else:
+            return ''
+res = res.style.applymap(highlight_significant, sign_level_1=0.05, sign_level_2=0.01)
+display(res)
